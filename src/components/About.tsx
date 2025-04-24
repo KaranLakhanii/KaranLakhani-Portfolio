75e6f84a7a2edc32d34a09e36f324e58395af0a1
Karan Lakhani <lakhanikaran025@gmail.com>
2025-04-25 02:01:13 +0530

    Add files via upload

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+        Dedicated interior designer student looking
+        for an opportunity to bring new creativity
+        skills and take more learning and experience.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
